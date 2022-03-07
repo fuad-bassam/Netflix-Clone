247c70d6e6415650b6618676e5a34c7a976cc5b0
fuad-bassam <f.man.x99@gmail.com>
2022-03-07 05:56:35 +0300

    v.1

--- netflixclone/src/App.jsx
+++ netflixclone/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Home from './components/Home/Home'
-
+import Favorite from './components/FavList/FavList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
 export default function App() {
 
 
   return (
-    <>
-   <Home/>
-
-    </>
+    // <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/fav" element={<Favorite />} />
+    </Routes>
   )
 }
--- netflixclone/src/components/Navbar/Navbar.jsx
+++ netflixclone/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,28 @@
-export default function Navbar(){
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
+
+export default function NavBar() {
+
+    return (
+
+
+        <>
+            <Navbar bg="danger" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Netflix</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/fav">Favorite</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
+
+
+    )
 
 
 };
